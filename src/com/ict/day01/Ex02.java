2e26be86e95dfddeae71a97be4898ee54252c3ae
nohssam73 <nohssam73@gmail.com>
2024-02-14 19:15:51 +0900

    Ex03 add2

--- src/com/ict/day01/Ex02.java
+++
@@ -1,7 +0,0 @@
-package com.ict.day01;
-
-public class Ex02 {
-	public static void main(String[] args) {
-		System.out.println("Ex02");
-	}
-}
--- src/com/ict/day01/Ex03.java
+++ src/com/ict/day01/Ex03.java
@@ -2,6 +2,6 @@ package com.ict.day01;
 
 public class Ex03 {
 	public static void main(String[] args) {
-		System.out.println("Ex03");
+		System.out.println("Ex03~~~");
 	}
 }
